1915091bec97bc23af7647a790c1ad9825297efd
MLigowski <mligowski698@technikumkreatywne.pl>
2025-05-21 14:03:47 +0200

    Komentarz

--- Lekcja 8/Zadanie1.cpp
+++ Lekcja 8/Zadanie1.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 using namespace std;
+int GetNumberFromUser() {
+int numerek;
+cout << "daj numer: ";
+cin >> numerek;
+	
+	return numerek;
+}
+int CalculateSumRecursive( int jedynka ) {
+  if ( jedynka == 1 ) 
+        return 1;
+    else
+	    return jedynka + CalculateSumRecursive(jedynka - 1);
 
-
-bool czyParzysta(int liczba) {
-    return (liczba % 2 == 0);
 }
 
+int DisplayResult( int tak ) {
+  int wynik = tak;
+	return wynik;
+}
 int main() {
-    int liczba;
-    cout << "Podaj liczbe calkowita: ";
-    cin >> liczba;
-    
-    if (czyParzysta(liczba)) {
-        cout << "Liczba " << liczba << " jest parzysta." << endl;
-    } else {
-        cout << "Liczba " << liczba << " jest nieparzysta." << endl;
-    }
-    
-    return 0;
+int numerek = GetNumberFromUser();
+int jedynka = CalculateSumRecursive(numerek);
+int wynik = DisplayResult(jedynka);
+
+	cout << "suma liczb od 1 do " << numerek << " wynosi: " << wynik << endl;
+return 0;
 }
